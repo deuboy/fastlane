4aaa9093cb16bdab54463a680f76cbba9589206e
criswang <cris89@taou.com>
2020-03-15 00:25:39 +0800

    rm install cert

--- cert/lib/cert/runner.rb
+++ cert/lib/cert/runner.rb
@@ -12,10 +12,10 @@ module Cert
     def launch
       run
 
-      installed = FastlaneCore::CertChecker.installed?(ENV["CER_FILE_PATH"], in_keychain: ENV["CER_KEYCHAIN_PATH"])
-      UI.message("Verifying the certificate is properly installed locally...")
-      UI.user_error!("Could not find the newly generated certificate installed", show_github_issues: true) unless installed
-      UI.success("Successfully installed certificate #{ENV['CER_CERTIFICATE_ID']}")
+      # installed = FastlaneCore::CertChecker.installed?(ENV["CER_FILE_PATH"], in_keychain: ENV["CER_KEYCHAIN_PATH"])
+      # UI.message("Verifying the certificate is properly installed locally...")
+      # UI.user_error!("Could not find the newly generated certificate installed", show_github_issues: true) unless installed
+      # UI.success("Successfully installed certificate #{ENV['CER_CERTIFICATE_ID']}")
       return ENV["CER_FILE_PATH"]
     end
 
@@ -196,10 +196,10 @@ module Cert
       cert_path = store_certificate(certificate, Cert.config[:filename])
 
       # Import all the things into the Keychain
-      keychain = File.expand_path(Cert.config[:keychain_path])
-      password = Cert.config[:keychain_password]
-      FastlaneCore::KeychainImporter.import_file(private_key_path, keychain, keychain_password: password)
-      FastlaneCore::KeychainImporter.import_file(cert_path, keychain, keychain_password: password)
+      # keychain = File.expand_path(Cert.config[:keychain_path])
+      # password = Cert.config[:keychain_password]
+      # FastlaneCore::KeychainImporter.import_file(private_key_path, keychain, keychain_password: password)
+      # FastlaneCore::KeychainImporter.import_file(cert_path, keychain, keychain_password: password)
 
       # Environment variables for the fastlane action
       ENV["CER_CERTIFICATE_ID"] = certificate.id
